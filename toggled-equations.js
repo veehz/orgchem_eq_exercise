618c738669df654cc1400ec5337143c159f10d3d
veehz <veehzhz@gmail.com>
2022-10-10 15:31:50 +0800

    add toggle and/or

--- toggled-equations.js
+++ toggled-equations.js
@@ -1,13 +1,27 @@
 let current_equations = equations;
 
+if (getCookie("ordering")) {
+  document.getElementById("ordering").innerText = getCookie("ordering");
+}
 document.getElementById("ordering").addEventListener("click", () => {
-    const btn = document.getElementById("ordering");
-    if(btn.innerText == "Ordered") btn.innerText = "Random";
-    else btn.innerText = "Ordered";
+  const btn = document.getElementById("ordering");
+  if (btn.innerText == "Ordered") btn.innerText = "Random";
+  else btn.innerText = "Ordered";
+  setCookie("ordering", btn.innerText);
 });
 
-function isRandom(){
-    return document.getElementById("ordering").innerText == "Random";
+if (getCookie("inclusion")) {
+  document.getElementById("inclusion").innerText = getCookie("inclusion");
+}
+document.getElementById("inclusion").addEventListener("click", () => {
+  const btn = document.getElementById("inclusion");
+  if (btn.innerText == "And") btn.innerText = "Or";
+  else btn.innerText = "And";
+  setCookie("inclusion", btn.innerText);
+});
+
+function isRandom() {
+  return document.getElementById("ordering").innerText == "Random";
 }
 
 const familyTypes = [];
@@ -16,23 +30,17 @@ for (const eq of equations) {
   if (!familyTypes.includes(eq.to)) familyTypes.push(eq.to);
 }
 
-// function checkToggleAll() {
-//   console.log("checkToggleAll");
-//   let allChecked = true;
-//   for (const family of familyTypes) {
-//     if (!document.getElementById(`toggle-${family}`).checked)
-//       allChecked = false;
-//   }
-//   if (allChecked) document.getElementById("toggle-family").checked = true;
-//   else document.getElementById("toggle-family").checked = false;
-// }
-
 function refreshCurrentEquations() {
   current_equations = [];
+  const inclusion = document.getElementById("inclusion").innerText;
   for (const eq of equations) {
     if (
-      document.getElementById(`toggle-${eq.from}`).checked &&
-      document.getElementById(`toggle-${eq.to}`).checked
+      (inclusion == "And" &&
+        document.getElementById(`toggle-${eq.from}`).checked &&
+        document.getElementById(`toggle-${eq.to}`).checked) ||
+      (inclusion == "Or" &&
+        (document.getElementById(`toggle-${eq.from}`).checked ||
+          document.getElementById(`toggle-${eq.to}`).checked))
     )
       current_equations.push(eq);
   }
@@ -46,18 +54,18 @@ for (const type of familyTypes) {
         <label for="toggle-${type}">${type}</label>
     </div>
     `;
+}
+
+for (const type of familyTypes) {
   if (getCookie(`toggle-${type}`) === "false") {
     document.getElementById(`toggle-${type}`).checked = false;
   }
-}
-
-for(const type of familyTypes){
-    document
+  document
     .getElementById(`toggle-${type}`)
     .addEventListener("change", (event) => {
       console.log("change");
       setCookie(`toggle-${type}`, event.target.checked ? "true" : "false");
-    //   checkToggleAll();
+      console.log(type, getCookie(`toggle-${type}`));
       refreshCurrentEquations();
       changeEquation();
     });
